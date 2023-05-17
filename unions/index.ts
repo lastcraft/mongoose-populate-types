52eaf219ecc946923bdb2caf68b382b70dfa703d
Marcus <marcus@lastcraft.com>
2023-05-17 22:38:33 +0100

    Discriminated unions

--- unions/index.ts
+++ unions/index.ts
@@ -1,11 +1,20 @@
 import mongoose, { HydratedDocument } from "mongoose";
-import { CareerModel, Career } from "./models/career";
+import {
+  CareerModel,
+  Career,
+  FiremanModel,
+  TeacherModel,
+  Fireman,
+  Teacher,
+} from "./models/career";
 
 const clearAll = async (): Promise<void> => {
   await CareerModel.deleteMany({});
 };
 
-const createChild = async (name: string): Promise<HydratedDocument<Career>> => {
+const createCareer = async (
+  name: string
+): Promise<HydratedDocument<Career>> => {
   const career = new CareerModel({
     name: name,
   });
@@ -23,10 +32,62 @@ const findCareer = async (
   return child;
 };
 
+const createFireman = async (
+  name: string,
+  temperature: number
+): Promise<HydratedDocument<Fireman>> => {
+  const career = new FiremanModel({
+    name,
+    temperature,
+  });
+  await career.save();
+  return career;
+};
+
+const findFireman = async (
+  query: mongoose.FilterQuery<Fireman>
+): Promise<HydratedDocument<Fireman>> => {
+  const child = await FiremanModel.findOne(query);
+  if (!child) {
+    throw new Error("Not found");
+  }
+  return child;
+};
+
+const createTeacher = async (
+  name: string,
+  books: number
+): Promise<HydratedDocument<Teacher>> => {
+  const career = new TeacherModel({
+    name,
+    books,
+  });
+  await career.save();
+  return career;
+};
+
+const findTeacher = async (
+  query: mongoose.FilterQuery<Teacher>
+): Promise<HydratedDocument<Teacher>> => {
+  const child = await TeacherModel.findOne(query);
+  if (!child) {
+    throw new Error("Not found");
+  }
+  return child;
+};
+
 const main = async (): Promise<number> => {
   await mongoose.connect("mongodb://localhost:27017/playpen");
   console.log("Connected");
   await clearAll();
+
+  console.log(await createFireman("fred", 451));
+  console.log(await createTeacher("tess", 1));
+  const fred = await findCareer({ name: "fred" });
+  if (fred && fred.type == "F") {
+    console.log(fred);
+    console.log(fred.temperature);
+  }
 
   await mongoose.disconnect();
   return 0;
--- unions/models/career.ts
+++ unions/models/career.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export interface Career {
   name: string;
+  type: "C";
 }
 
 const careerSchema = new mongoose.Schema<Career>({
@@ -9,3 +10,31 @@ const careerSchema = new mongoose.Schema<Career>({
 });
 
 export const CareerModel = mongoose.model<Career>("Career", careerSchema);
+
+export interface Fireman extends Career {
+  temperature: number;
+  type: "F";
+}
+
+const firemanSchema = new mongoose.Schema({
+  temperature: { type: Number, required: true },
+});
+
+export const FiremanModel = CareerModel.discriminator<Fireman>(
+  "Fireman",
+  firemanSchema
+);
+
+export interface Teacher extends Career {
+  books: number;
+  type: "T";
+}
+
+const teacherSchema = new mongoose.Schema({
+  books: { type: Number, required: true },
+});
+
+export const TeacherModel = CareerModel.discriminator<Teacher>(
+  "Teacher",
+  teacherSchema
+);
