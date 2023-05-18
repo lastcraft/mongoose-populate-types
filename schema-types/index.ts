1cccac1c10af55e98a6f368e6d6ea5c8b2c78c48
Marcus <marcus@lastcraft.com>
2023-05-18 14:31:24 +0100

    Playing with the schema types

--- schema-types/index.ts
+++ schema-types/index.ts
@@ -26,10 +26,25 @@ const main = async (): Promise<number> => {
   console.log("Connected");
   await clearAll();
 
-  console.log(await createA({ name: "aaa", bObject: { s: "bbb", n: 1 } }));
-  const a = await findA({ name: "aaa" });
-  if (a) {
-    console.log(a);
+  console.log(await createA({ name: "a1", bObject: { s: "b1", n: 1 } }));
+  const a1 = await findA({ name: "a1" });
+  if (a1) {
+    console.log(a1);
+  }
+
+  console.log(await createA({ name: "a2", bObject: { s: "b2" } }));
+  const a2 = await findA({ name: "a2" });
+  if (a2) {
+    console.log(a2);
+  }
+
+  console.log(
+    await createA({ name: "a3", bObject: { s: "b3" }, cObject: { s: "c3" } })
+  );
+  const a3 = await findA({ name: "a3" });
+  if (a3) {
+    console.log(a3);
+    console.log(a3.cObject?.s);
   }
 
   await mongoose.disconnect();
--- schema-types/models/a-model.ts
+++ schema-types/models/a-model.ts
@@ -3,13 +3,18 @@ import mongoose from "mongoose";
 export interface A {
   name: string;
   nickname?: string;
-  bObject: { s: string; n: number };
+  bObject: { s: string; n?: number };
+  cObject?: { s: string; n?: number };
 }
 
 const aSchema = new mongoose.Schema<A>({
   name: { type: String, required: true },
   nickname: { type: String, required: false },
-  bObject: { s: String, n: Number },
+  bObject: { s: String, n: { type: Number, required: false } },
+  cObject: {
+    type: { s: String, n: { type: Number, required: false } },
+    required: false,
+  },
 });
 
 export const AModel = mongoose.model<A>("A", aSchema);
